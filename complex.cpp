2c6e0e6e24249c6a337dca695ae865bdd25fb665
xnmktsxt <32427318+xnmktsxt@users.noreply.github.com>
2018-03-26 01:55:02 +0800

    cpp version

--- complex.cpp
+++ complex.cpp
@@ -1,49 +1,95 @@
-/***********************************************
- * Discription: four operation of plural numbers
- * Date: 2018-03-20 20:30:00
- * Author: Ming Yi
- ***********************************************/
+/*******************************************
+    Description: complex arithmetic
+    Date: 2018-03-26 01:07:00
+    Author: Ming Yi
+    Student ID: 17080701008
+*******************************************/
+
 #include <iostream>
 using namespace std;
 
-class complex
+//declaring a complex structure
+typedef struct
 {
-    int real;//real part
-    int imag;//imaginary part
+    int real;
+    int imag;
+}complex;
 
-    complex add(complex a, complex b)
-    {
-        complex temp;
-        temp.imag = a.imag + b.imag;
-        temp.real = a.real + b.real;
-        return temp;
-    }
-
-    complex sub(complex a, complex b)
-    {
-        complex temp;
-        temp.imag = a.imag - b.imag;
-        temp.real = a.real - b.real;
-        return temp;
-    }
-
-
-};
+//overloaded operator: +, -, *
+complex operator + (complex &a, complex &b);
+complex operator - (complex &a, complex &b);
+complex operator * (complex &a, complex &b);
 
 int main()
 {
-    cout << "Enter a plural numbers, please." << endl;
-    cout << "Real part:";
-    complex comp1;//instantiate the first plural
-    cin >> comp1.real;
-    cout << "Imaginary part:";
-    cin >> comp1.imag;
-    
-    
+    complex comp1, comp2, result;//declaring a variable of complex structure type
+    cout << "Please enter the real and imaginary parts of a complex number(integer): ";
+    cin >> comp1.real >> comp1.imag;
+    cout << "Please enter the real and imaginary parts of a complex number(integer): ";
+    cin >> comp2.real >> comp2.imag;
+    char c_operator;//declaring the variable of operator
+    cout << "Please enter the operator: ";
+    cin >> c_operator;
 
+    //Find the corresponding operator operation.
+    //When an incorrect operator is entered, 
+    //give a prompt and ask for it again.
+    while(true)
+    {
+        if(c_operator == '+')
+        {
+            result = comp1 + comp2;
+            break;
+        }
+        else if (c_operator = '-')
+        {
+            result = comp1 - comp2;
+            break;
+        }
+        else if(c_operator = '*')
+        {
+            result = comp1 * comp2;
+            break;
+        }
+        else
+        {
+            cout << "Sorry, your entered an incorrect operator!"
+                 << "Please try again." << endl;
+            continue;
+        }
+    }
+    //If the complex imaginary part is less than zero, 
+    //the plus sign will not be printed
+    if(result.imag >= 0)
+        cout << "The result of the operation is: " << result.real 
+             << "+" << result.imag << "i" << endl;
+    else
+        cout << "The result of the operation is: " << result.real 
+             << result.imag << "i" << endl;
+     
     return 0;
 }
 
+complex operator + (complex &a, complex &b)
+{
+    complex temp;
+    temp.real = a.real + b.real;
+    temp.imag = a.imag + b.imag;
+    return temp;
+}
 
+complex operator - (complex &a, complex &b)
+{
+    complex temp;
+    temp.real = a.real - b.real;
+    temp.imag = a.imag - b.imag;
+    return temp;
+}
 
-
+complex operator * (complex &a, complex &b)
+{
+    complex temp;
+    temp.real = a.real * b.real - a.imag * b.imag;
+    temp.imag = a.imag * b.real + a.real * b.imag;
+    return temp;
+}
